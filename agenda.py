39ff878d43c7ba466f272774640916e8e1d866a1
ibjaro <ibjaro@gmail.com>
2023-10-27 18:44:55 -0600

    Creacion de clase funciones para la agenda

--- agenda.py
+++ agenda.py
@@ -1,20 +1,59 @@
 # programa de agenda, probando.
 
-nombre = input("Ingrese su nombre: ")
-apellido = input("Ingrese su apellido: ")
-edad = input("Ingrese su edad: ")
-telefono = input("Ingrese su telefono: ")
-email = input("Ingrese su email: ")
-direccion = input("Ingrese su direccion: ")
-ciudad = input("Ingrese su ciudad: ")
-pais = input("Ingrese su pais: ")
+class Agenda:
+    def __init__(self):
+        self.contactos = []
 
-print("\nDatos ingresados: ")
-print("Nombre: ", nombre)
-print("Apellido: ", apellido)
-print("Edad: ", edad)
-print("Telefono: ", telefono)
-print("Email: ", email)
-print("Direccion: ", direccion)
-print("Ciudad: ", ciudad)
-print("Pais: ", pais)
+    def menu(self):
+        print("1. Agregar contacto")
+        print("2. Eliminar contacto")
+        print("3. Mostrar contacto")
+        print("4. Salir")
+
+    def agregar(self):
+        print("Agregar contacto")
+        nombre = input("Ingrese su Nombre: ")
+        apellido = input("Ingrese su Apellido: ")
+        telefono = input("Ingrese su Telefono: ")
+        correo = input("Ingrese su Correo: ")
+        direccion = input("Ingrese su direccion: ")
+        self.contactos.append([nombre, apellido, telefono, correo, direccion])
+
+    def eliminar(self):
+        print("Eliminar contacto")
+        nombre = input("Ingrese el Nombre de contacto a eliminar: ")
+        for i in self.contactos:
+            if nombre in i:
+                self.contactos.remove(i)
+                print("Contacto eliminado")
+            else:
+                print("Contacto no encontrado")
+
+    def mostrar(self):
+        print("Mostrar contactos")
+        for i in self.contactos:
+            print(i)
+
+    def salir(self):
+        print("Gracias por usar la Agenda, hasta luego!")
+
+    def principal(self):
+        while True:
+            self.menu()
+            opcion = int(input("Opcion: "))
+            if opcion == 1:
+                self.agregar()
+            elif opcion == 2:
+                self.eliminar()
+            elif opcion == 3:
+                self.mostrar()
+            elif opcion == 4:
+                self.salir()
+                break
+            else:
+                print("Opcion no valida")
+
+agenda = Agenda()
+agenda.principal()
+
+
